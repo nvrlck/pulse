7767eaa2b9063d5d49f5fe9e92beb93211c9a0b2
nvrlck <107360287+nvrlck@users.noreply.github.com>
2022-07-21 20:43:36 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,165 @@
+// let number = 8;
+// const pi = 3.14;
+
+// const { eq } = require("semver");
+
+// number
+// string - "", '', ``
+// true/false 
+// null 
+// undefined
+// let obj = {
+//     name: 'apple',
+//     color: 'red',
+//     weight: 200
+// }
+// Symbol
+
+// let answer = confirm("Vam est' 18?")
+// console.log(answer);
+
+// let answer = prompt("Vam est' 18?", "")
+// console.log(answer);
+
+// console.log(4 + 'fasd');
+
+// let isChecked = true,
+//     isClose = true;
+
+// console.log(isChecked && isClose);
+// console.log(isChecked || isClose);
+
+// if (2*4 == 8*1) {
+//     console.log("right")
+// } else {
+//     console.log("false")
+// }
+
+// let answer = confirm("Vam est' 18?");
+// if (answer) {
+//     console.log('Welcome')
+// } else {
+//     console.log("Leave")
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(i);
+// }
+
+// function logging(a, b) {
+//     console.log( a + b );
+// }
+// logging(3, 5);
+
+// logging(6, 8);
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive : [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        })
+    };
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+
+    function valideForms(form) {
+        $(form).validate({
+            rules: {
+                name: "required",
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: "Пожалуйста, введите свое имя",
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введена почта"
+                }
+            }
+        });
+    };
+
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    // Smooth scroll and page up
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    new WOW().init();
+
+});
